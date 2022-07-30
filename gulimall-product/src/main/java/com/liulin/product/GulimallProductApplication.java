de5fe346bae6b94a12aa5719da1f88bfbbd5ed0d
0x3c <liulin0x3c@gmail.com>
2022-07-31 02:45:16 +0800

    OSS阿里云服务文件上传

--- gulimall-product/src/main/java/com/liulin/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/liulin/product/GulimallProductApplication.java
@@ -1,14 +1,8 @@
 package com.liulin.product;
 
-import com.aliyun.oss.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 @EnableDiscoveryClient
 @SpringBootApplication
---
+++ gulimall-third-party/src/test/java/com/liulin/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,18 @@
+package com.liulin.thirdparty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class GulimallThirdPartyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
